53f1d9010827929c2cb3f13fed540312b08f41ba
mcdanton <danhefter@gmail.com>
2020-06-11 13:18:06 -0400

    Added views for add recipe page

---
+++ recipe-book/AddRecipeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddRecipeViewController.swift
+//  recipe-book
+//
+//  Created by Dan Hefter on 6/10/20.
+//  Copyright © 2020 Dan Hefter. All rights reserved.
+//
+
+import UIKit
+
+class AddRecipeViewController: UIViewController {
+    
+    @IBOutlet weak var recipeEditImageButton: UIButton!
+    @IBOutlet weak var recipeImageView: UIImageView!
+    @IBOutlet weak var recipeNameLabel: UITextField!
+    @IBOutlet weak var recipeCookTimeLabel: UITextField!
+    @IBOutlet weak var recipeIngredientsTextView: UITextView!
+    @IBOutlet weak var recipeInstructionsTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
